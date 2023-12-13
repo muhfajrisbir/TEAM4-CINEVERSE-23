1dea492fdd06a0a8f6132d70ca4a4a1d01fb6fcb
muhfajrisbir <mhmmadfajri@gmail.com>
2023-12-13 08:12:58 +0700

    Update alert message

--- hapus.php
+++ hapus.php
@@ -5,5 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["id"])) {
   $id = $_GET["id"];
   $conn = connectDB();
   $conn->query("DELETE FROM movies WHERE id=$id");
-  header("Location: watchlist.php");
+  echo "<script>alert('Film Berhasil dihapus.');</script>";
+  // Mengalihkan ke watchlist
+  echo '<script>window.location.href = "watchlist.php";</script>';
 }
